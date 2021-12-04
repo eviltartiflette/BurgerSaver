c3a8c60c7c8cad48fd7ef343bc9401f326439bd7
Maxime Rancon <maximerancon@gmail.com>
2021-12-04 15:06:33 +0100

    added csv upload

--- js/index.js
+++ js/index.js
@@ -1,8 +1,139 @@
+let userData={
+    "orders": null,
+    "restaurants": null
+}
+
+function handleOrderSheetButton(){
+    orderSheetFileSelect.click()
+}
+
+function handleRestaurantSheetButton(){
+    restaurantSheetFileSelect.click()
+}
+
+
+function handleOrderSheetUpload(event){
+    orderSheetUploadButton.disabled = true
+    let reader = new FileReader();
+    reader.readAsText(event.target.files[0], "UTF-8");
+    reader.onload = function (fevt) {
+        try {
+            const parsed = $.csv.toObjects(fevt.target.result)
+            if(validateOrders(parsed)){
+                userData.orders = parsed
+                modalError("")
+                refreshProcessButton()
+            }
+            else{
+                throw new Error("Error while parsing orders")
+            }
+
+        } catch (error) {
+            orderSheetUploadButton.disabled = false
+            modalError("Error while reading file: "+error)
+        }
+    }
+    reader.onerror = function (fevt) {
+        orderSheetUploadButton.disabled = false
+        modalError("Error while reading file: "+fevt)
+    }
+}
+
+function handleRestaurantSheetUpload(event){
+    restaurantSheetUploadButton.disabled = true
+    let reader = new FileReader();
+    reader.readAsText(event.target.files[0], "UTF-8");
+    reader.onload = function (fevt) {
+        try {
+            const parsed = $.csv.toObjects(fevt.target.result)
+            if(validateRestaurants(parsed)){
+                userData.restaurants = parsed
+                modalError("")
+                refreshProcessButton()
+            }
+            else{
+                throw new Error("Error while parsing restaurants")
+            }
+
+        } catch (error) {
+            restaurantSheetUploadButton.disabled = false
+            modalError("Error while reading file: "+error)
+        }
+    }
+    reader.onerror = function (fevt) {
+        restaurantSheetUploadButton.disabled = false
+        modalError("Error while reading file: "+fevt)
+    }
+}
+
+
+function modalError(txt){
+    const selector = document.getElementById('modalError')
+    selector.innerText = txt
+}
+
+function validateOrders(orders){
+    try {
+        const fields = ['Currency','Customizations','Item Name','Item Price','Order ID','Order Price','Order Status','Order Time','Restaurant ID','Special Instructions','Territory']
+        for (let i = 0; i < fields.length; i++) {
+            if(!Object.keys(orders[0]).includes(fields[i])){
+                return false
+            }
+        }
+    } catch (error) {
+        return false
+    }
+    return true
+}
+
+function validateRestaurants(restaurants){
+    try {
+        const fields = ['City','Restaurant ID','Restaurant Name','Order Time']
+        for (let i = 0; i < fields.length; i++) {
+            if(!Object.keys(restaurants[0]).includes(fields[i])){
+                return false
+            }
+        }
+    } catch (error) {
+        return false
+    }
+    return true
+}
+
+function refreshProcessButton(){
+    if(userData.restaurants !== null && userData.orders !== null){
+        processButton.disabled = false
+    }
+}
+
+function handleProcessButton(){
+    alert('ok')
+}
+
 function init(){
+    processButton.disabled = true
     const initModal = new bootstrap.Modal(document.getElementById('initModal'), {
         keyboard: false
     })
     initModal.toggle()
 }
 
-init()
+const orderSheetUploadButton = document.getElementById('orderSheetUploadButton')
+const restaurantSheetUploadButton = document.getElementById('restaurantSheetUploadButton')
+
+const orderSheetFileSelect = document.getElementById('orderSheetFile')
+const restaurantSheetFileSelect = document.getElementById('restaurantSheetFile')
+
+const processButton = document.getElementById('processButton')
+
+orderSheetUploadButton.addEventListener('click', handleOrderSheetButton)
+orderSheetFileSelect.addEventListener('change', handleOrderSheetUpload)
+
+restaurantSheetUploadButton.addEventListener('click', handleRestaurantSheetButton)
+restaurantSheetFileSelect.addEventListener('change', handleRestaurantSheetUpload)
+
+processButton.addEventListener('click', handleProcessButton)
+
+$(document).ready(function () {
+    init()
+});
